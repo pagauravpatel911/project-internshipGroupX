ee30f3221bab538cb22781efc733a2b72bdc4097
gaurav <pgauravpatel911@gmail.com>
2022-03-21 16:49:08 +0530

    initial commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,30 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const AuthorController= require("../controllers/AuthersController")
-const BookController= require("../controllers/bookController")
+const AuthorController= require("../controllers/AuthersController");
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-// router.post("/createUser", UserController.createUser  )
 
-// router.get("/getUsersData", UserController.getUsersData)
-
-// router.post("/createBook", BookController.createBook  )
-
-// router.get("/getBooksData", BookController.getBooksData)
-
-// router.post("/updateBooks", BookController.updateBooks)
-// router.post("/deleteBooks", BookController.deleteBooks)
-
-//MOMENT JS
-// const moment = require('moment');
-// router.get("/dateManipulations", function (req, res) {
     
-//     // const today = moment();
-//     // let x= today.add(10, "days")
-
-//     // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-//     // console.log(validOrNot)
-    
-//     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-//     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-//     let x= dateB.diff(dateA, "days")
-//     console.log(x)
-
-//     res.send({ msg: "all good"})
+router.post("/createAuther",AuthorController.createAuther);
+router.post("/createBooks",AuthorController.createBooks);
+router.get("/booksByChetanBhagat",AuthorController.booksByChetanBhagat);
+router.get("/updatePrice" , AuthorController.upadatePrice1 );
+router.get("/pricesBooks", AuthorController.pricesBooks);
 
 
-//})
-//   1-03-2022 Assignment
-    
 
- router.post("/createBookData",BookController.createBookData);
- router.post("/createAuther", AuthorController.createAuther);
- router.get("/findChetanBhagatBooks", AuthorController.findChetanBhagatBooks);
-router.get("/changeAndUpdate", AuthorController.changeAndUpdate);
-router.get("/authorName", AuthorController.authorName);
+
+
+//  router.post("/createBookData",AuthorController.createNewBook);
+ 
+//  router.get("/allBooks", AuthorController.allBooks);
+// router.get("/updateBookPrice", AuthorController.upadatedBookPrice);
+// router.get("/authorsName", AuthorController.authorsName);
 
 
 
